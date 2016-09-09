ca011d8cc8cf009e4c3ba751448c5ebae5d534d3
vincent <113933579@qq.com>
2016-09-09 16:35:39 +0800

    fragment SharedElement(待优化)

--- textfivedotzero/src/com/example/textfivedotzero/MainActivity.java
+++ textfivedotzero/src/com/example/textfivedotzero/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.textfivedotzero;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+
+	private Context mContext;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mContext = MainActivity.this;
+		
+		 if(savedInstanceState == null){
+	        getFragmentManager().beginTransaction().add(R.id.mFrameLayout, new FragmentContent())
+	        .commit();
+	     }
+		
 	}
-	
 }
